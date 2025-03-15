9b34b9eabcc546be9cae2f7c80637c8d9168898e
rSanches35 <rafaelsanches.s35@gmail.com>
2025-03-15 17:04:05 -0300

    Estrutura da Página

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+ import { Helmet } from 'react-helmet';
+ 
+ import './Home.css';
+ 
+ const Home = () => {
+ 
+     return (
+         <>
+            <Helmet><title>COAFNOR</title></Helmet>
+
+            
+         </>
+     );
+ }; export default Home;
